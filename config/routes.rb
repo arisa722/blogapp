f6238fcad7e2d922dc1ba6703bf6162e3b5f88cc
梅森安璃彩 <>
2026-08-04 13:17:58 +0900

    day8 articles controller に変更など

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root to: 'home#index'
-  get '/' => 'home#index'
+  root to: 'articles#index'
   get '/about' => 'home#about'
 end
