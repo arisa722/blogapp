c8d3acef7b4dab9cbd3f509a4978ca15f15e9c3c
梅森安璃彩 <>
2026-09-08 15:46:27 +0900

    いいね機能の追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,6 +14,8 @@
 #  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
+    has_one_attached :eyecatch
+    
     validates :title, presence: true
     validates :title, length: { minimum: 2, maximum: 100 }
     validates :title, format: { with: /\A(?!\@)/ }
@@ -32,6 +34,7 @@ class Article < ApplicationRecord
     # end
 
     has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
     belongs_to :user
 
     def display_created_at
@@ -40,6 +43,10 @@ class Article < ApplicationRecord
 
     def author_name
         user.display_name
+    end
+
+    def like_count
+        likes.count
     end
 
 end
